5ad815459ebd16bfa48464c9336346a18c631b1f
Thomas <Thomas@192.168.0.33>
2018-10-31 18:37:42 +0100

    minor

--- resources/views/formcontents/create.blade.php
+++ resources/views/formcontents/create.blade.php
@@ -1,19 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    
 
                      <?php
          echo Form::open(array('url' => 'formcontents'));
@@ -36,10 +23,5 @@
             echo Form::submit('Submit');
          echo Form::close();
       ?>
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+               
 @endsection
