a79770db8d572a6a652f0df459e2e55260604a2c
cyrill94 <cyrill.fueglister@students.fhnw.ch>
2018-11-04 23:58:01 +0100

    myobject page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="{{ URL::asset('assets/fonts/ionicons.min.css')}}">
     <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/tenant_font_design.css')}}">
 </head>
 
 <body>
