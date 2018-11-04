3b1a94931b115b3eb8eb5f36e57ee332b239046a
cyrill94 <cyrill.fueglister@students.fhnw.ch>
2018-11-05 00:49:04 +0100

    about us & assets

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,45 @@
 @extends('layouts.app')
 @section('content')
 
-<h1> About </h1>
-        <p> This is the laravel application from ...</p>
+<div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Über uns</h4>
+        </div>
+    </div>
+    <main class="page lanidng-page"></main>
+    <div style="padding-bottom: 20px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8"><img class="img-fluid" src="assets/img/about_us.jpg" style="padding-top: 5px;"></div>
+                <div class="col-md-4">
+                    <p class="text-left" style="padding-top: 5px;"><strong>Unsere Vision</strong></p>
+                    <p class="text-left">Wir möchten Ihnen den Lifestyle powered by Atomstrom vermitteln. Vertrauen Sie uns wenn es um Atomstrom wie auch Immobilien geht! Dank unserem breiten Tätigkeitsfeld können wir Ihr Haus direkt an das Atomkraftwerk anschliessen.<br>Spätestens
+                        damit, bringen wir auch Sie zum strahlen.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="container m-auto">
+            <div class="row">
+                <div class="col-md-3"><img src="assets/img/mrburns.jpg" width="120" height="120" align="middle" class="rounded-circle img-fluid align-items-center align-self-center m-auto" />
+                    <p><strong>Mr. Burns</strong><br>Geschäftsführer<br>Alter: unbekannt<br>Motto: ausgezeichnet</p>
+                </div>
+                <div class="col-md-3"><img class="rounded-circle img-fluid" src="assets/img/smithers.jpg" width="120px" height="120px">
+                    <p><strong>Waylon Smithers</strong><br>Reinigungskraft<br>Alter: 45<br>Motto: Stets zu Diensten</p>
+                </div>
+                <div class="col-md-3"><img class="rounded-circle img-fluid" src="assets/img/lisasimpson.png" width="120px" height="120px">
+                    <p><strong>Lisa Simpson</strong><br>Assistentin<br>Alter: 8<br>Motto: Carpe diem</p>
+                </div>
+                <div class="col"><img class="rounded-circle img-fluid" src="assets/img/cletus_spuckler.png" width="120px" height="120px">
+                    <p><strong>Cletus Spuckler</strong><br>Strahlenschutz<br>Alter: unbekannt<br>Motto: Beweis es!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div></div>
+
+
+
+
 @endsection
