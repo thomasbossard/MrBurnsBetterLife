3d6cfaa0bcd9ef0f65fa88df065579dcf7e16f7d
cyrill94 <cyrill.fueglister@students.fhnw.ch>
2018-11-04 17:03:18 +0100

    app.blade.php menubar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,96 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Mr. Burns Betterlife AG</title>
+    
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">    
+    <link rel="stylesheet" href="{{ URL::asset('assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/fontawesome-all.min.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/ionicons.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
 </head>
+
 <body>
     <div id="app">
-         <ul class="navbar-nav mr-auto">
-             <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            
          
-              <li class="nav-item">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Startseite
-                </a> 
-              </li>
-                 
-                      
-              <li class="nav-item">
-                <div class="container">
-                <a class="navbar-brand" href="{{ url('/about') }}">
-                    Über uns
-                </a>
-              </li>
-              
-              <li class="nav-item">
-                <div class="container">
-                <a class="navbar-brand" href="{{ url('/rentableobjects') }}">
-                    Objekte
-                </a>
-              </li>
-                
-              <li class="nav-item">
-                <div class="container">
-                <a class="navbar-brand" href="{{ url('/services') }}">
-                    Services
-                </a>
-              </li> 
-              
-              <li class="nav-item">
-                <div class="container">
-                <a class="navbar-brand" href="{{ url('/myobject') }}">
-                    Mein Objekt
-                </a>
-              </li> 
-                
-           
-         
-                  
-              
-                      
-                 
-            
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        <section class="portfolio-block block-intro">
+            <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
+                <div class="container"><a class="navbar-brand logo" href="#">Mr Burns Betterlife AG</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div
+                        class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="/">Startseite</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="rentableobjects">Objekte</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="about">Über uns</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="formcontents">Kontakt</a></li>
+                            
+                            @auth
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="myobject">Mein Objekt</a></li>
+                            @endauth
+                            
+                            @guest
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="{{ route('login') }}"><i class="fas fa-sign-in-alt"></i>{{ __('    Login') }}</a></li>
+                            <li class="nav-item" role="presentation">@if (Route::has('register'))
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 @endif
                             </li>
-                        @else
-                            <li class="nav-item dropdown">
+                           
+                            @else
+                                <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
@@ -106,17 +57,25 @@
                                         @csrf
                                     </form>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                </li>
+                            @endguest
+     
+                        </ul>
                 </div>
-            </div>
-                       </ul>
-        </nav>
-
+                </div>
+            </nav>
+        </section>
+         
+    </div>
+      
         <main class="py-4">
             @yield('content')
         </main>
-    </div>                    
+                      
+    <script src="{{ URL::asset('assets/js/jquery.min.js')}}"></script>
+    <script src="{{ URL::asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
+    <script src="{{ URL::asset('assets/js/theme.js')}}"></script>
+    
 </body>
 </html>
