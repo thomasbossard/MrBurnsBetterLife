2e593736194b2ac24d9b24dc40289566b67ccf28
cyrill94 <cyrill.fueglister@students.fhnw.ch>
2018-11-26 00:57:49 +0100

    newinvoice design

--- resources/views/manage/newinvoice.blade.php
+++ resources/views/manage/newinvoice.blade.php
@@ -8,42 +8,63 @@
 @endif 
 
 @if(!$users->isEmpty())
-    Neue Rechnung erfassen
-    <br>
+
+    <div class="container">
+        <h3>Neuen Rechnung erfassen</h3>
+    </div>
     
+    <div class="container">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Mieter</th>
+                        <th>Rechnungstyp</th>
+                        <th>Betrag</th>
+                        <th>Beschreibung</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                        <select name="user_id" form="newinvoice">
+                            @foreach ($users as $user)
+                                <option value="{{$user->id}}">{{$user->name}} {{$user->givenname}}</option>
+                            @endforeach
+                        </select>
+                        </td>
+                        
+                        <td>
+                        <select name="type_id" form="newinvoice">
+                            @foreach ($invoicetypes as $invoicetype)
+                                <option value="{{$invoicetype->id}}">{{$invoicetype->type}}</option>
+                            @endforeach
+                        </select>
+                        </td>
+                        
+                        <td>
+                            <input type="number" name="amount" form="newinvoice">
+                        </td>
+                            
+                        <td>
+                            <input type="text" name="description" form="newinvoice">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
     
-    Benutzer: <select name="user_id" form="newinvoice">
-        @foreach ($users as $user)
-            <option value="{{$user->id}}">{{$user->name}} {{$user->givenname}}</option>
-        @endforeach
-    </select>
-    <br>
-    
-    
-    Rechnungstyp: <select name="type_id" form="newinvoice">
-        @foreach ($invoicetypes as $invoicetype)
-            <option value="{{$invoicetype->id}}">{{$invoicetype->type}}</option>
-        @endforeach
-    </select>
-    <br>
-    
-    
-    Betrag: <input type="number" name="amount" form="newinvoice">
-    <br>
-    
-    
-    Beschreibung: <input type="text" name="description" form="newinvoice">
-    <br>
-    
-    
-    Datei: <input type="file" id="fileupload" name="fileupload" form="newinvoice">
-        <br><br>
-    
+
+<div class="container">    
+    <input type="file" id="fileupload" name="fileupload" form="newinvoice">
     
     <form action="/newinvoice" id="newinvoice" method="post" enctype="multipart/form-data">
         {{ csrf_field() }}
         <input type="submit" value="Neue Rechnung erfassen">
     </form>
+</div>
+
 @endif
 
 @endsection
