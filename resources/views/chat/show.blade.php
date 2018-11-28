640008c3c68e0fe51557b27f874066b6ffbbadc2
cyrill94 <cyrill.fueglister@students.fhnw.ch>
2018-11-28 23:52:33 +0100

    Chat header geändert

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -4,14 +4,15 @@
 
     
     <meta name="friendId" content="{{ $friend->id }}">
+    
+    <div class="container" style="padding-bottom:10px">
+        <h3>Chat mit {{ $friend->name }} {{ $friend->givenname }}</h3>
+    </div>
+    
     <div class="container">
         <div class="column is-8 is-offset-2">
             <div class="panel">
-                <div class="panel-heading">
-                  
-                    <div class="container" style="padding-bottom:10px">
-                        <h3>Chat mit {{ $friend->name }} {{ $friend->givenname }}</h3>
-                    </div>
+                <div class="panel-heading">                  
                     <chat v-bind:chats="chats" v-bind:userid="{{ Auth::user()->id }}" v-bind:friendid="{{ $friend->id }}"></chat>
                 </div>
             </div>
